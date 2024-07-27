fbffda43547fb50c1e363bb451b6e8e78689d846
dev-prakhar <prakhar02ver@gmail.com>
2024-07-27 13:08:12 +0530

    fetched articles using newsAPI

--- src/components/newsitem.js
+++ src/components/newsitem.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-  
   render() {
-    let { title, description, imageUrl } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
-        <div className="card my-3" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card my-3" style={{ width: "18rem" }}>
+          <img
+            src={
+              !imageUrl
+                ? "https://png.pngtree.com/png-vector/20190820/ourmid/pngtree-no-image-vector-illustration-isolated-png-image_1694547.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-              {description}
-            </p>
-            <a href="/newsdetail" className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
