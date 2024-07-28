a5e3f25c84d1c2e7289d0b22c3593a7a60998deb
dev-prakhar <149324096+Prakhar0202@users.noreply.github.com>
2024-07-28 23:43:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ export default class App extends Component {
       <Router>
         <Navbar/>
         <Routes>
-          <Route  path="/" element={<News key="general" pageSize={5} country="in" category="general" />} />
+          <Route  path="/general" element={<News key="general" pageSize={5} country="in" category="general" />} />
           <Route  path="/business" element={<News key="business" pageSize={5} country="in" category="business" />} />
           <Route  path="/entertainment" element={<News key="entertainment" pageSize={5} country="in" category="entertainment" />} />
           <Route  path="/health" element={<News key="health" pageSize={5} country="in" category="health" />} />
-          <Route  path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports" />} />
+          <Route  path="/" element={<News key="sports" pageSize={5} country="in" category="sports" />} />
           <Route  path="/technology" element={<News key="technology" pageSize={5} country="in" category="technology" />} />
         </Routes>
       </Router>
